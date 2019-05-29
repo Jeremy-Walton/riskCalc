9a5689d722ef91a8cae1e811b2daa5576c11deef
Jeremy Walton <jeremy.walton@rolemodelsoftware.com>
2019-05-29 15:02:04 -0400

    Simplify calculate method

--- player.rb
+++ player.rb
@@ -1,17 +1,19 @@
 class Player
-  attr_accessor :name, :ratio, :luck, :roles, :wins, :losses, :luckwins, :lucklosses
+  attr_accessor :name, :ratio, :luck, :wins, :losses, :luckwins, :lucklosses
   def initialize(name)
     @name = name
     @luck = 0
     @luckwins = 0
     @lucklosses = 0
-    @roles = 0
-    @wins =  0
+    @wins = 0
     @losses = 0
     @ratio = 0
+
+
   end
 
   def calculate(win, die1, die2, position)
+    # position (a or d) is not used yet
     if win
       print @name + ' beat ' # Should be moved to the calculator since the end of this sentance depends on the other player
       @wins += 1
@@ -22,35 +24,7 @@ class Player
     end
 
     luck_attribute = win ? @luckwins : @lucklosses
-    if position == 'a'
-      if die1 == 3 && die2 == 1
-        luck_attribute += 0.33
-      elsif die1 == 3 && die2 == 2
-        luck_attribute += 0.67
-      elsif die1 == 2 && die2 == 1
-        luck_attribute += 0.5
-      elsif die1 == 2 && die2 ==2
-        luck_attribute += 1
-      elsif die1 == 1 && die2 == 1
-        luck_attribute += 1
-      elsif die1 == 1 && die2 == 2
-        luck_attribute += 2
-      end
-    else
-      if die1 == 2 && die2 == 1
-        luck_attribute += 0.5
-      elsif die1 == 2 && die2 == 2
-        luck_attribute += 1
-      elsif die1 == 2 && die2 == 3
-        luck_attribute += 1.33
-      elsif die1 == 1 && die2 ==1
-        luck_attribute += 1
-      elsif die1 == 1 && die2 == 2
-        luck_attribute += 2
-      elsif die1 == 1 && die2 == 3
-        luck_attribute += 1.67
-      end
-    end
+    luck_attribute += (die2.to_f / die1.to_f).round(2)
 
     @ratio = @wins / @losses.to_f
     @luck = @luckwins / @lucklosses.to_f
