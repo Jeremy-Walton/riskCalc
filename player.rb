7997489889b5ec765d68cda2974cda89ccdc7b2f
Josh McLeod <josh@mcleodfamily.org>
2019-05-30 12:23:43 -0400

    Rebase from master

--- player.rb
+++ player.rb
@@ -12,15 +12,26 @@ class Player
 
   end
 
-  def calculate(win, die1, die2, position)
+  def calculate(win, die1, die2, position, undo)
     # position (a or d) is not used yet
     if win
-      print @name + ' beat ' # Should be moved to the calculator since the end of this sentance depends on the other player
-      @wins += 1
+        if undo
+          print "The roll where " + @name + ' beat '
+          @wins -= 1
+        else
+          print @name + ' beat ' # Should be moved to the calculator since the end of this sentance depends on the other player
+          @wins += 1
+        end
     else
-      puts @name + " in a #{die1} to #{die2} role. " + @name + random_message # Should be moved to the calculator since the end of this sentance depends on the other player
-      puts ''
-      @losses += 1
+      if undo 
+        puts @name + " in a #{die1} to #{die2} role has been undone."
+        puts ""
+        @losses-=1
+      else
+        puts @name + " in a #{die1} to #{die2} role. " + @name + random_message # Should be moved to the calculator since the end of this sentance depends on the other player
+        puts ''
+        @losses += 1 
+      end
 
     end
 
