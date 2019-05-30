6d88a25daba9a64325846fb54ec2d219383d8b8c
Josh McLeod <josh@mcleodfamily.org>
2019-05-30 13:17:12 -0400

    Fix

--- player.rb
+++ player.rb
@@ -15,13 +15,12 @@ class Player
   def calculate(win, die1, die2, position, undo)
     # position (a or d) is not used yet
     if win
-          print @name + ' beat ' # Should be moved to the calculator since the end of this sentance depends on the other player
-          @wins += 1
+      print @name + ' beat ' # Should be moved to the calculator since the end of this sentance depends on the other player
+      @wins += 1
     else
-
-        puts @name + " in a #{die1} to #{die2} role. " + @name + random_message # Should be moved to the calculator since the end of this sentance depends on the other player
-        puts ''
-        @losses += 1 
+      puts @name + " in a #{die1} to #{die2} role. " + @name + random_message # Should be moved to the calculator since the end of this sentance depends on the other player
+      puts ''
+      @losses += 1 
     end 
     luck_modifier = (die2.to_f / die1.to_f).round(2)
 
@@ -37,12 +36,12 @@ class Player
 
   def calculate_undo(win, die1, die2, position)
     if win
-          print "The roll where " + @name + ' beat '
-          @wins -= 1
+      print "The roll where " + @name + ' beat '
+      @wins -= 1
     else
-        puts @name + " in a #{die1} to #{die2} role has been undone."
-        puts ""
-        @losses-=1
+      puts @name + " in a #{die1} to #{die2} role has been undone."
+      puts ""
+      @losses-=1
     end
 
     luck_modifier = (die2.to_f / die1.to_f).round(2)
