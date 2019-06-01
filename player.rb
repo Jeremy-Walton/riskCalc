9fa037b39c3b2e8f43f0b5458fb0145e14809c0b
Jeremy Walton <jeremy.walton@rolemodelsoftware.com>
2019-05-31 22:59:39 -0400

    Clean up methods and remove duplication (#7)

--- player.rb
+++ player.rb
@@ -9,58 +9,53 @@ class Player
     @losses = 0
     @ratio = 0
   end
-  
+
   def calculate(win, die1, die2, position)
     # position (a or d) is not used yet
     if win
-      print @name + ' beat ' # Should be moved to the calculator since the end of this sentance depends on the other player
+      print "#{@name} beat " # Should be moved to the calculator since the end of this sentance depends on the other player
       @wins += 1
     else
-      puts @name + " in a #{die1} to #{die2} role. " + @name + random_message # Should be moved to the calculator since the end of this sentance depends on the other player
+      puts "#{@name} in a #{die1} to #{die2} roll. #{@name} #{random_message}" # Should be moved to the calculator since the end of this sentance depends on the other player
       puts ''
       @losses += 1
     end
 
     luck_modifier = (die2.to_f / die1.to_f).round(2)
-
-    if win
-      @luckwins += luck_modifier
-    else
-      @lucklosses += luck_modifier
-    end
-
-    @ratio = @wins / @losses.to_f
-    @luck = @luckwins / @lucklosses.to_f
+    update_values(win, luck_modifier, undo: false)
   end
 
   def calculate_undo(win, die1, die2, position)
     if win
-      print "The roll where " + @name + ' beat '
+      print "The roll where #{@name} beat "
       @wins -= 1
     else
-      puts @name + " in a #{die1} to #{die2} role has been undone."
-      puts ""
+      puts "#{@name} in a #{die1} to #{die2} roll has been undone."
+      puts ''
       @losses-=1
     end
 
-     luck_modifier = (die2.to_f / die1.to_f).round(2)
-
-     if win
-      @luckwins -= luck_modifier
-    else
-      @lucklosses -= luck_modifier
-    end
-
-    @ratio = @wins / @losses.to_f
-    @luck = @luckwins / @lucklosses.to_f
+    luck_modifier = (die2.to_f / die1.to_f).round(2)
+    update_values(win, -luck_modifier, undo: true)
   end
 
   def random_message
     case rand(1..4)
-    when 1; ' needs to quit sucking.'
-    when 2; ' should get more luck.'
-    when 3; ' should just give up at this point.'
-    else; ' is the big unluck.'
+    when 1; 'needs to quit sucking.'
+    when 2; 'should get more luck.'
+    when 3; 'should just give up at this point.'
+    else; 'is the big unluck.'
     end
+  end
+
+  def update_values(win, value, undo: false)
+    if win
+      @luckwins += value
+    else
+      @lucklosses += value
+    end
+
+    @ratio = @wins / @losses.to_f
+    @luck = @luckwins / @lucklosses.to_f
   end
 end
