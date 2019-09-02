47adcdb8c1d72914a43879279c533ca4a89eb624
Jeremy Walton <jthelegoman@gmail.com>
2019-09-02 18:19:14 -0400

    Clear whitespace

--- models/risk_calculator.rb
+++ models/risk_calculator.rb
@@ -32,43 +32,43 @@ class RiskCalculator
     new_roll = Roll.new(player1, player2, die1, die2)
     @rolls.push(new_roll)
     log_message(new_roll)
-    
+
     calculate(player1, player2, die1, die2)
   end
-  
+
   def calculate(player1, player2, die1, die2)
     luck = (die2.to_f / die1.to_f).round(2)
-    
+
     player1.one_to_three_wins += 1  if(die1 == 1 && die2 == 3)
     player2.three_to_one_losses += 1  if(die1 == 1 && die2 == 3)
     player1.update_win(luck: luck)
     player2.update_loss(luck: luck)
   end
-  
+
   def calculate_undo(player1, player2, die1, die2)
     luck = (die2.to_f / die1.to_f).round(2)
-    
+
     player1.one_to_three_wins -= 1  if(die1 == 1 && die2 == 3)
     player2.three_to_one_losses -= 1  if(die1 == 1 && die2 == 3)
     player1.update_win(luck: luck, undo: true)
     player2.update_loss(luck: luck, undo: true)
   end
-  
+
   def undo_roll
     if @rolls.any?
       roll = @rolls.pop
-      
+
       calculate_undo(roll.player1, roll.player2, roll.die1, roll.die2)
       log_message_undo
     else
       puts "No rolls yet\n"
     end
   end
-  
+
   def log_message(roll)
     @log_messages.push(roll.to_s)
   end
-  
+
   def log_message_undo
     if !@log_messages.empty?
       @log_messages.pop
@@ -76,20 +76,20 @@ class RiskCalculator
       puts 'There are no messages to undo!'
     end
   end
-  
+
   def logs
     @log_messages
   end
-  
+
   def rolls
     @rolls
   end
-  
+
   def random_scenario(player_num, roll_num)
     @players = []
     @random_names = []
     initialize_names(player_num.to_i)
-    
+
     while roll_num > 0
       player1 = find_or_add_player(@random_names.sample)
       player2 = find_or_add_player(@random_names.sample)
@@ -99,7 +99,7 @@ class RiskCalculator
       end
     end
   end
-  
+
   def streak(player)
     if @streak_holder == player.name
       player.streak_count += 1
@@ -114,7 +114,7 @@ class RiskCalculator
 
   def find_or_add_player(name)
     player = @players.find { |p| p.name == name }
-    
+
     if !player || @players.empty?
       puts "New player (#{name}) added", ''
       player = Player.new(name)
